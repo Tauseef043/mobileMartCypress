ddcc227952e65568cf4381c43e98cfdec3f1496f
tauseef043 <Muhammadtauseef043@gmail.com>
2022-12-01 09:39:09 +0500

    1 decmeber 2022

--- cypress/e2e/tests/HomePageTest.cy.js
+++ cypress/e2e/tests/HomePageTest.cy.js
@@ -1,5 +1,4 @@
 /// <reference types="Cypress" />
-import { trim } from "cypress/types/lodash/index.js";
 import HomePageLeftNav from "../pageObjects/HomePageLeftNavOB.cy.js";
 
 describe('HomePage Test Suit',function(){
@@ -30,10 +29,44 @@ describe('HomePage Test Suit',function(){
 
 
     })
-    it('leftNav Visiblity',function(){
-       leftNav.getLeft_fruitAndeg(this.data.fruitNav).should('have.text',' Fruits & Vegetables')
+  
+    it('left fruit Nav Visiblity',function(){
+    leftNav.getLeft_fruitAndeg(this.data.fruitNav).then(function(element){
+
+        const navTitle=element.text();
+        expect(navTitle.includes(this.data.fruitNav)).to.be.true
+
+      })
+
+      //  leftNav.getLeft_fruitAndeg(this.data.fruitNav).should('have.text',' Fruits & Vegetables')
     })
 
+    it('left fruit sub Nav Visiblity',function(){
+      leftNav.getLeft_fruitAndeg(this.data.fruitNav).invoke('show')
+      cy.contains('Cuts & Sprouts').click({force: true} )
+      cy.url().should('include','cuts-sprouts')
+      
+  
+      cy.pause()
+        //  leftNav.getLeft_fruitAndeg(this.data.fruitNav).should('have.text',' Fruits & Vegetables')
+      })
+
+
+    it('left meat Nav Visiblity',function(){
+      leftNav.getLeft_fruitAndeg(this.data.meatNac).then(function(element){
+  
+          const navTitle=element.text();
+          expect(navTitle.includes(this.data.meatNac)).to.be.true
+  
+        })
+      })
+      it('left breakfast Nav Visiblity',function(){
+        leftNav.getLeft_fruitAndeg(this.data.breakfast).then(function(element){
     
+            const navTitle=element.text();
+            expect(navTitle.includes(this.data.breakfast)).to.be.true
+    
+          })
+        })
 
 })
--- cypress/e2e/tests/test.cy.js
+++ cypress/e2e/tests/test.cy.js
@@ -2,5 +2,9 @@ describe('sad',function(){
 
     it('sa',function(){
         cy.visit('www.google.com')
+
+
+        cy.get("input[name='q']").type('test')
+        
     })
 })
